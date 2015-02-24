1fa1971a85f0a97aa0699bd2ad4576f061c376c1
jasmo2 <jasmo2.com@gmail.com>
2015-02-24 12:57:01 -0500

    ¿Email?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   # set delivery method to :smtp, :sendmail or :test
   config.action_mailer.delivery_method = :smtp
-
+ 
   config.action_mailer.smtp_settings = {
   address:        'smtp.gmail.com',
   authentication: 'plain',
